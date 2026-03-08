8ad07e2056241dcbabfd8645aedadde21eab633c
Rai-web-create <ride_irmrmn618@yahoo.co.jp>
2026-03-08 13:01:29 +0900

    first upload

--- script.js
+++ script.js
@@ -11,3 +11,35 @@ window.onload = function() {
   const text = title.innerText;
   title.innerHTML = "";
   typeEffect(title, text, 80);};
+ const elements = document.querySelectorAll(".fade-up");
+
+window.addEventListener("scroll", () => {
+  elements.forEach((el) => {
+    const position = el.getBoundingClientRect().top;
+    const screenPosition = window.innerHeight * 0.8;
+
+    if (position < screenPosition) {el.classList.add("show");}});});
+const light = document.querySelector(".light");
+
+document.addEventListener("mousemove", (e) => {
+  light.style.left = e.pageX + "px";
+  light.style.top = e.pageY + "px";});
+
+const sections = document.querySelectorAll('.fade-section');
+
+window.addEventListener('scroll', () => {
+  const trigger = window.innerHeight * 0.8;
+
+  sections.forEach(section => {const top = section.getBoundingClientRect().top;
+      if(top < trigger){ section.classList.add('show');}});});
+const works = document.querySelectorAll(".work-item");
+
+window.addEventListener("scroll",()=>{
+
+const trigger = window.innerHeight*0.85;
+
+works.forEach(item=>{
+const top = item.getBoundingClientRect().top;
+
+if(top<trigger){
+item.classList.add("show");}});});
